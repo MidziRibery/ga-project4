2420525c119766f66c6228cabbd7f35944c1c071
Midzi Ribery <muhd.termidzi@gmail.com>
2023-01-23 11:30:27 +0800

    setup m

--- server/index.js
+++ server/index.js
@@ -21,4 +21,29 @@ app.use(morgan("common"));
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
-app.use("/assets", express.static(path.join(__dirname, "public.assets")));
+app.use("/assets", express.static(path.join(__dirname, "public.assets"))); //set the directory of where we keep our assets aka images,locally.
+
+/* FILE STORAGE */
+
+//this is how someone saves a file. Eveytime someone uploads a file onto the website,
+//it is gonna be saved into public/assets.
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, "public/assets");
+    },
+    filename: function (req, file, cb) {
+        cb(null, file.originalname);
+    }
+});
+// these configurations and information can be found in GitHuB repo of Multer.
+
+const upload = multer({ storage }); // this will help to save the file
+
+/*Setup Mongoose*/
+const PORT = process.env.port || 6001;
+mongoose.connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}).then(() => {
+    app.listen(PORT, () => console.log(`Listening on Server Port ${PORT}`));
+}).catch((error) => console.log(`${error} did not connect`));
