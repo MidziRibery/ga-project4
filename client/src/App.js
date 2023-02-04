b7bb1e7b0fc0b39b183e42f024cee5f4c4dc3d7e
Midzi Ribery <muhd.termidzi@gmail.com>
2023-02-04 21:13:24 +0800

    testing

--- client/src/App.js
+++ client/src/App.js
@@ -6,26 +6,27 @@ import { useMemo } from "react";
 import { useSelector } from "react-redux";
 import { CssBaseline, ThemeProvider } from '@mui/material';
 import { createTheme } from '@mui/material/styles';
-import { themeSettings } from 'theme';
+import { themeSettings } from './theme.js';
 
 
 
 function App() {
 //add state and selector
-const mode = useSelector((state) => state.mode); // help grab value that we set in initial state under state/index.js
-// const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
+const mode = useSelector((state) => state.mode); // help grab value that we set in initial state under state/index.js into the correct reducer
+const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]); // to set the theme
 
   return (
     <div className="app">
       <BrowserRouter>
-      {/* <ThemeProvider theme={theme}> */}
-        <Routes>
-          <Route path='/' element={<LoginPage/>} />
-          <Route path='/home' element={<HomePage/>} />
-          <Route path='/profile/:usedId' element={<ProfilePage/>} />
-        </Routes>
-        {/* </ThemeProvider> */}
-      </BrowserRouter>
+        <ThemeProvider theme={theme}>
+          <CssBaseline/>
+          <Routes>
+            <Route path='/' element={<LoginPage/>} />
+            <Route path='/home' element={<HomePage/>} />
+            <Route path='/profile/:usedId' element={<ProfilePage/>} />
+          </Routes>
+          </ThemeProvider>
+        </BrowserRouter>
     </div>
   );
 }
