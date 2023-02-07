7e312a6b3b13c37215caf0fe4ab7c736f04d329c
Midzi Ribery <muhd.termidzi@gmail.com>
2023-02-07 21:36:18 +0800

    start on login page

--- client/src/scenes/loginPage/index.jsx
+++ client/src/scenes/loginPage/index.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
+import { Box, Typography, useTheme, useMediaQuery } from "@mui/material"
 
 const LoginPage = () => {
+  const theme = useTheme(); //to use the colours
+  const isNonMobileScreens = useMediaQuery("(min)")
   return (
     <div>LoginPage11</div>
   )
