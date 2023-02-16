6df1466a75e1db40af0652f846554b6ae131d036
Midzi Ribery <muhd.termidzi@gmail.com>
2023-02-16 17:55:59 +0800

    profile page update

--- client/src/scenes/profilePage/index.jsx
+++ client/src/scenes/profilePage/index.jsx
@@ -10,6 +10,26 @@ import PostsWidget from "scenes/widgets/PostsWidget";
 import UserWidget from "scenes/widgets/UserWidget";
 
 const ProfilePage = () => {
+    const [user, setUser] = useState(null);
+    const { userId } = useParams();
+    const token = useSelector((state) => state.token);
+    const isNonMobileScreens = useMediaQuery("(min-width:1000px)");
+  
+    const getUser = async () => {
+      const response = await fetch(`http://localhost:3001/users/${userId}`, {
+        method: "GET",
+        headers: { Authorization: `Bearer ${token}` },
+      });
+      const data = await response.json();
+      setUser(data);
+    };
+  
+    useEffect(() => {
+      getUser();
+    }, []); // eslint-disable-line react-hooks/exhaustive-deps
+  
+    if (!user) return null;
+
     return(
         <div>Profile Page</div>
     )
