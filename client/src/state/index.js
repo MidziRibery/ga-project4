f429aa693aae10416c4a649fc2aa915cdc2fcc77
Midzi Ribery <muhd.termidzi@gmail.com>
2023-02-16 17:54:56 +0800

    profile page

--- client/src/state/index.js
+++ client/src/state/index.js
@@ -43,7 +43,7 @@ export const authSlice = createSlice({
         },
         setPost: (state, action) => {
             const updatedPosts = state.posts.map((post) => {
-                if (post._id === action.payload.post_id) return action.payload.post; //only update the post that was changed
+                if (post._id === action.payload.post._id) return action.payload.post; //only update the post that was changed
                 return post;
             });
             state.posts = updatedPosts; 
