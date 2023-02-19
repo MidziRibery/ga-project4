e096939abf301ec974e027f31566bddfc99b315b
Midzi Ribery <muhd.termidzi@gmail.com>
2023-02-19 10:37:54 +0800

    picture access working server

--- server/index.js
+++ server/index.js
@@ -31,7 +31,7 @@ app.use(morgan("common"));
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
-app.use("/assets", express.static(path.join(__dirname, "public.assets"))); //set the directory of where we keep our assets aka images,locally.
+app.use("/assets", express.static(path.join(__dirname, "public/assets"))); //set the directory of where we keep our assets aka images,locally.
 app.use("/auth", authRoutes)
 /* FILE STORAGE */
 
